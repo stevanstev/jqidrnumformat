aecf71dcda9fe6d72a3c79eab8c1f2d7e4634e8f
stevanstev <37262169+stevanstev@users.noreply.github.com>
2019-12-18 17:33:00 +0700

    jqnumformat.js

---
+++ jqnumformat.js
@@ -0,0 +1,54 @@
+let formattingMoney = (isCustom,separatorStr="",startCustom="",endCustom="") => {
+  let formatting = $(".formatting");
+
+  let recursiveNumber = (number,length,separator) => {
+      let result = "";
+      let i;
+      let flagging = 0;
+
+      for(i = length - 1; i >= 0 ; i--){
+          if(flagging == 3){
+              //for adding separator
+              result = number[i]+separator+result;
+              flagging = 1;
+          }else{
+              result = number[i] + result;
+              flagging += 1;
+          }
+      }
+
+      return result;
+  }
+
+  let countMoney = (money,length,separator) => {
+      let result;
+
+      if(length == 4){
+          restNumber = money[1]+money[2]+money[3];
+          result = money[0]+separator+restNumber;
+      }else{
+          result = recursiveNumber(money,length,separator);
+      }
+
+      return result;
+  }
+
+  //Invalid if user added number after endCustom string
+  let getMoney = isCustom === 1 ? formatting.val().split(endCustom)[0] : getMoney;
+  //Remove all non digit for processing
+  getMoney = getMoney.replace(/[^0-9]/g,"");
+  let getLength = getMoney.length;
+  let result;
+
+  if(getLength > 3){
+      result = countMoney(getMoney,getLength,separatorStr);
+  }else{
+      result = getMoney;
+  }
+
+  if(isCustom === 1){
+      $(".formatting").val(startCustom+result+endCustom);
+  }else{
+      $(".formatting").val(result);
+  }
+}
