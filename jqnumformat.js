58a4414ff74d89175f96f39d83d675e7e2ba47ed
stevanstev <37262169+stevanstev@users.noreply.github.com>
2019-12-18 18:29:09 +0700

    Update jqnumformat.js

--- jqnumformat.js
+++ jqnumformat.js
@@ -1,12 +1,4 @@
 let formattingMoney = (isCustom=0,separatorStr="",startCustom="",endCustom="") => {
-  let formatting = $(".formatting");
-    //Invalid if user added number after endCustom string
-  let getMoney = isCustom === 1 ? formatting.val().split(endCustom)[0] : formatting.val();
-  //Remove all non digit for processing
-  getMoney = getMoney.replace(/[^0-9]/g,"");
-  let getLength = getMoney.length;
-  let result;
-
   let recursiveNumber = (number,length,separator) => {
       let result = "";
       let i;
@@ -25,7 +17,7 @@ let formattingMoney = (isCustom=0,separatorStr="",startCustom="",endCustom="") =
 
       return result;
   }
-
+  
   let countMoney = (money,length,separator) => {
       let result;
 
@@ -38,6 +30,33 @@ let formattingMoney = (isCustom=0,separatorStr="",startCustom="",endCustom="") =
 
       return result;
   }
+
+  let checkStartWith = (money) => {
+    //Check if money starts with 0
+    let i;
+    let result;
+    let moneySize = money.length;
+    for(i = 0 ; i < moneySize ; i++){
+      if(money[i] === "0"){
+        continue;
+      }else{
+        result = money.substr(i,moneySize - 1);
+        break;
+      }
+    }
+
+    return result;
+  }
+
+
+  let formatting = $(".formatting");
+    //Invalid if user added number after endCustom string
+  let getMoney = isCustom === 1 ? formatting.val().split(endCustom)[0] : formatting.val();
+  //Remove all non digit for processing
+  getMoney = getMoney.replace(/[^0-9]/g,"");
+  getMoney = getMoney.startsWith("0") ? checkStartWith(getMoney) : getMoney;
+  let getLength = getMoney.length;
+  let result;
 
   if(getLength > 3){
       result = countMoney(getMoney,getLength,separatorStr);
